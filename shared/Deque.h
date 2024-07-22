1fc9559cc3cb94536544dc18a2da9f82e408c588
GCgang <powm97@gmail.com>
2024-07-22 15:49:15 +0900

    Feat: Deque

---
+++ shared/Deque.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "Queue.h"
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+
+// Double Ended Queue
+template<typename T>
+class Deque : public Queue<T>
+{
+    typedef Queue<T> Base;
+
+    public:
+        Deque(int capacity)
+            : Queue<T>(capacity)
+        {}
+
+        T& Front()
+        {
+            return Base::Front();
+        }
+
+        T& Back()
+        {
+            return Base::Rear();
+        }
+
+        void PushFront(const T& item)
+        {
+            if (Base::IsFull())
+                Base::Resize();
+            Base::mQueue[Base::mFront] = item;
+            Base::mFront = (Base::mFront - 1 + Base::mCapacity) % Base::mCapacity;
+        }
+
+        void PushBack(const T& item)
+        {
+            Base::Enqueue(item);
+        }
+
+        void PopFront()
+        {
+            Base::Dequeue();
+        }
+
+        void PopBack()
+        {
+            assert(!Base::IsEmpty());
+            Base::mRear = (Base::mRear - 1 + Base::mCapacity) % Base::mCapacity;
+        }
+    private:
+        T* mQueue;
+        int mCapacity;
+        int mFront;
+        int mRear;
+        bool mPrintDebug;
+};
